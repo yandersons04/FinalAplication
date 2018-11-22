0995f4976d7a933a52f87d5b7fbcaade2ca548e6
Yata <yataschalke04@gmail.com>
2018-11-22 21:17:00 -0200

    Segundo commit do Projeto Final.

--- app/src/main/java/com/finalaplication/tecinfo/finalaplication/MainActivity.java
+++ app/src/main/java/com/finalaplication/tecinfo/finalaplication/MainActivity.java
@@ -1,13 +1,43 @@
 package com.finalaplication.tecinfo.finalaplication;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        int id = menuItem.getItemId();
+        if (id == R.id.configuracao) {
+            Toast.makeText(getApplicationContext(), "Abrir Configurações", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.calcularmedia) {
+            Toast.makeText(getApplicationContext(), "Abrir Calcular Média", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.calculadora) {
+            Toast.makeText(getApplicationContext(), "Abrir Calculadora", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.chamadaFalsa) {
+            Toast.makeText(getApplicationContext(), "Abrir Chamada Falsa", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.perfil) {
+            Toast.makeText(getApplicationContext(), "Abrir Perfil", Toast.LENGTH_SHORT).show();
+        }
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+
     }
 }
